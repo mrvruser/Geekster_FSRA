23f1b79df91a2cfb616e25c170548113007d300c
Kabir <sagithedeveloper@gmail.com>
2023-02-08 21:58:59 +0530

    added get endpoint

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var express = require("express");
 var cors = require("cors");
 var mongodb = require("mongodb");
 const mongoClient = mongodb.MongoClient;
+const port = 8000;
 var app = express();
-let port = 8000
 let dburl =
   "mongodb+srv://kabir:kabir21@cluster0.chfc7rh.mongodb.net/?retryWrites=true&w=majority";
 app.use(cors());
@@ -12,6 +12,35 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server Started Succe");
 }); //http://localhost:8000/
+
+app.get("/data", (req, res) => {
+  mongoClient.connect(dburl, (err, cluster) => {
+    if (err) {
+      console.log("Connection Failed to connect with Database");
+      res.json({
+        OK: false,
+        data: "Connection Failed to connect with Database",
+      });
+    } else {
+      let db = cluster.db("FSAR");
+      var collectionInsightRef = db.collection("Insightcarddata");
+      collectionInsightRef.find({}).toArray((err, insightData) => {
+        if (err) {
+          console.log("Error while Getting the data insight data");
+          res.json({
+            OK: false,
+            data: "Error while Getting the data",
+          });
+        } else {
+          res.json({
+            OK: true,
+            data: insightData,
+          });
+        }
+      });
+    }
+  });
+});
 
 app.post("/login", (req, res) => {
   //http://localhost:8000/login
@@ -29,9 +58,7 @@ app.post("/login", (req, res) => {
       let db = cluster.db("FSAR");
       let collectionRef = db.collection("users");
       collectionRef.find().toArray((err, info) => {
-        console.log("sssssssss", info);
         if (err) {
-          console.log("error whileconnecting db");
           res.json({
             OK: false,
             data: "Something Went Wrong",
@@ -42,23 +69,27 @@ app.post("/login", (req, res) => {
           });
 
           if (newData.length > 0) {
-            var collectionInsightRef = db.collection("Insightcarddata");
-            collectionInsightRef.find({}).toArray((err, insightData) => {
-              if (err) {
-                console.log("Error while Getting the data insight data");
-                res.json({
-                  OK: false,
-                  data: "Error while Getting the data",
-                });
-              } else {
-                res.json({
-                  OK: true,
-                  data: insightData,
-                });
-              }
+            res.json({
+              Ok: true,
+              data: newData,
+              message: "Valid User",
             });
+            // var collectionInsightRef = db.collection("Insightcarddata");
+            // collectionInsightRef.find({}).toArray((err, insightData) => {
+            //   if (err) {
+            //     console.log("Error while Getting the data insight data");
+            //     res.json({
+            //       OK: false,
+            //       data: "Error while Getting the data",
+            //     });
+            //   } else {
+            //     res.json({
+            //       OK: true,
+            //       data: insightData,
+            //     });
+            //   }
+            // });
           } else {
-            console.log("no user");
             res.json({
               Ok: false,
               data: "Please Check the Credentails",
